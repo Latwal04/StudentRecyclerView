073ac400bca8e518ef7a2a56512e0b6f45aa95de
latwal04 <39549263+latwal04@users.noreply.github.com>
2018-06-14 19:39:33 +0530

    Initial commit

---
+++ app/src/main/java/com/brilicaservices/studentrecyclerview/StudentModel.java
@@ -0,0 +1,9 @@
+package com.brilicaservices.studentrecyclerview;
+public class StudentModel {
+
+    String studentName;
+
+    public StudentModel(String studentName) {
+        this.studentName = studentName;
+    }
+}
